acccbc887fc8a253a56be2aa368f33a928c2e762
Serhat <ulucs15@itu.edu.tr>
2022-08-16 19:46:17 +0300

    First commit

---
+++ StaffManagement/Controllers/StaffController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    [Route("/[controller]s")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        static List<Staff> StaffList = new List<Staff>() {new Staff
+        (){
+             id = 1,
+             name = "Deny",
+             lastname = "Sellen",
+             dateOfBirth = new DateTime(1989,01,01),
+             email = "deny@gmail.com",
+             phoneNumber = "555443366",
+             salary = 4450
+
+        }};
+        
+
+        [HttpGet]
+        public List<Staff> GetStaffs()
+        {
+            var staffList = StaffList.OrderBy(x => x.id).ToList<Staff>();
+            return staffList;
+        }
+
+        [HttpGet("{Id}")]
+        public Staff GetStaffsById([FromQuery] int id)
+        {
+            var staff = StaffList.SingleOrDefault(x => x.id == id);
+            return staff;
+        }
+
+        [HttpPost]
+        public IActionResult AddStaff([FromBody]Staff newStaff)
+        {
+            var staff = StaffList.SingleOrDefault(x => x.name == newStaff.name);
+
+            if (staff is not null)
+            {
+                return BadRequest();
+            }
+
+            StaffList.Add(newStaff);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStaff(int id)
+        {
+            var staff = StaffList.SingleOrDefault(x => x.id == id);
+            if(staff is null)
+            {
+                return BadRequest();
+            }
+            StaffList.Remove(staff);
+            return Ok();
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStaff(int id,[FromQuery] Staff updatedStaff)
+        {
+            var staff = StaffList.Single(x => x.id == updatedStaff.id);
+            if(updatedStaff is null)
+            {
+                return BadRequest();
+            }
+           staff.name = updatedStaff.name != default ? updatedStaff.name:staff.name;
+           staff.lastname = updatedStaff.lastname != default ? updatedStaff.lastname : staff.lastname;
+           staff.email = updatedStaff.email != default ? updatedStaff.email : staff.email;
+           staff.dateOfBirth = updatedStaff.dateOfBirth != default ? updatedStaff.dateOfBirth : staff.dateOfBirth;
+           staff.phoneNumber = updatedStaff.phoneNumber != default ? updatedStaff.phoneNumber : staff.phoneNumber;
+           staff.salary = updatedStaff.salary != default ? updatedStaff.salary : staff.salary;
+           return Ok();
+        }
+    }
+}
---
+++ StaffManagement/Staff.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    public class Staff 
+    {
+        public int id { get; set; }
+
+        [StringLength(maximumLength:250,MinimumLength =3,ErrorMessage ="Name length must be in range of 3-250")]
+        [RegularExpression(@"^[a-z/A-Z]*", ErrorMessage = "Special characters and numbers are not allowed.")]
+        public string name { get; set; }
+
+        [StringLength(maximumLength: 250, MinimumLength = 3, ErrorMessage = "Lastname length must be in range of 3-250")]
+        [RegularExpression(@"^[a-z/A-Z]*",ErrorMessage = "Special characters and numbers are not allowed.")]
+        public string lastname { get; set; }
+
+        [Range(typeof(DateTime), "1/2/2004", "3/4/2004",
+        ErrorMessage = "Value for {0} must be between {1} and {2}")]
+        [DataType(DataType.Date)]
+        public DateTime dateOfBirth { get; set; }
+
+
+        [RegularExpression("^[a-zA-Z/(.@)]*",ErrorMessage="Special characters are not allowed except '.'")]
+        [EmailAddress]
+        public string email { get; set; }
+
+
+        [RegularExpression(@"^((\d{2})-(\d{3})-(\d{3})-(\d{2})-(\d{2}))$", ErrorMessage = "Error")]
+        public string phoneNumber { get; set; }
+
+
+
+        [Range(minimum:2000, maximum:9000, ErrorMessage = "please enter a value between 2000 - 9000")]
+        public double salary { get; set; }
+
+       
+    }
+}
